2da5b4e6fe22716425c8728265dffb82ba0e6bcf
Shazil0204 <141632242+Shazil0204@users.noreply.github.com>
2024-03-20 14:08:02 +0100

    Update App.tsx

--- ReactTraining1/src/App.tsx
+++ ReactTraining1/src/App.tsx
@@ -2,8 +2,7 @@ import Navbar from "./Components/Navbar";
 
 export default function App() {
   return (
-    <div className="h-screen bg-indigo-950/85 text-white font-mono">
-      <div><Navbar /></div>
-    </div>
+    
+      <Navbar />
   );
 }
