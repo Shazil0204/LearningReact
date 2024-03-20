b32c74aa16d3b51fa9b426e1116bc263b73aaea9
Shazil0204 <141632242+Shazil0204@users.noreply.github.com>
2024-03-20 14:08:00 +0100

    Update Navbar.tsx

--- ReactTraining1/src/Components/Navbar.tsx
+++ ReactTraining1/src/Components/Navbar.tsx
@@ -1,5 +1,18 @@
-function Navbar() {
-  return <div className="w-full text-center text-2xl hover:scale-90 duration-300 hover:bg-gray-100 hover:text-black rounded-full cursor-pointer">Navbar</div>;
+export default function Navbar() {
+  let navlinks = ["Home", "About", "Services", "Contact"];
+  const navStyle =
+    "mx-5 w-32 text-center p-2 bg-gray-300/25 rounded-lg hover:bg-gray-300/50 duration-300 cursor-pointer font-thin border border-transparent hover:border-blue-500 hover:scale-95";
+  return (
+    <div className="h-screen bg-indigo-950/85 p-2 font-mono">
+      <div className="flex justify-center text-white text-2xl rounded-lg">
+        <div className="flex">
+          {navlinks.map((nav) => (
+            <div className={navStyle} key={nav}>
+              {nav}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default Navbar;
